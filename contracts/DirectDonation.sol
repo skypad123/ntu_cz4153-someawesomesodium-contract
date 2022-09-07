557f9f59991944c44f6cced0ba6798436837d7d4
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-09-07 22:06:28 +0800

    added untested modified contracts

--- contracts/DirectDonation.sol
+++ contracts/DirectDonation.sol
@@ -1,64 +1,119 @@
-
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.7.0 <0.9.0;
 
-import "./PercentRecord.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-
+import "./HitchensUnorderedAddressSet.sol";
 interface IDirectDonation {
+
+     struct SimplifiedFormat {
+        address walletAddress; 
+        uint32 percentAllocation;
+    }
+
     function createAllocation( address _walletAddress, uint32 _initPercentCount ) external;
     function addAllocation( address _walletAddress, uint32 _percentCount ) external; 
     function subtractAllocation( address _walletAddress, uint32 _percentCount ) external; 
     function getAllocationValue( address _walletAddress ) external returns(uint32); 
+    function getAllocationSum()external returns(uint32);
     function removeAllocation( address _walletAddress ) external;
 
     function donate() external payable; 
     function withdrawContractBalance() external;
+    function destory() external;
+
+    event LogCreateAllocation(address indexed _walletAddress, uint32 _percentCount);
+    event LogAddAllocation(address indexed  _walletAddress, uint32 _PercentCount);
+    event LogSubtractAllocation(address indexed _walletAddress, uint32 _percentCount);
+    event LogRemoveAllocation (address indexed _walletAddress);
+    event LogDonation( uint256 sum );
+    event LogWithdrawal( uint256 sum );
+    event LogDestruction();
 }
 
-contract DirectDonation is PercentRecord, Ownable, IDirectDonation {
+contract DirectDonation is IDirectDonation,Ownable {
 
-    constructor(address  origin) Ownable(){
-        Ownable._transferOwnership(origin);
+    using HitchensUnorderedAddressSetLib for HitchensUnorderedAddressSetLib.Set;
+    HitchensUnorderedAddressSetLib.Set PercentSet;
+
+    mapping(address => uint32) PercentAllocationMap;
+    uint32 PercentAllocationSum;
+
+    constructor(address _walletOwner) Ownable(){
+        Ownable._transferOwnership(_walletOwner);
+        PercentAllocationSum = 0; 
     }
 
-    function createAllocation( address _walletAddress, uint32 _initPercentCount ) public override(IDirectDonation, PercentRecord) onlyOwner {
-        PercentRecord.createAllocation( _walletAddress, _initPercentCount);
+    function createAllocation( address _walletAddress, uint32 _initPercentCount ) external onlyOwner {
+        require(PercentAllocationSum + _initPercentCount <= 1000000000, "total percent count cannot pass 100%.");
+        PercentSet.insert(_walletAddress);
+        PercentAllocationMap[_walletAddress] = _initPercentCount;
+        PercentAllocationSum += _initPercentCount;
+        emit LogCreateAllocation( _walletAddress, _initPercentCount);
     }
-    function addAllocation( address _walletAddress, uint32 _percentCount ) public override(IDirectDonation, PercentRecord) onlyOwner{
-        PercentRecord.addAllocation( _walletAddress, _percentCount);
+    function addAllocation( address _walletAddress, uint32 _percentCount ) external onlyOwner {
+        require(PercentAllocationSum + _percentCount <= 1000000000, "total percent count cannot pass 100%.");
+        PercentAllocationMap[_walletAddress] += _percentCount;
+        PercentAllocationSum += _percentCount;
+        emit LogAddAllocation( _walletAddress, _percentCount);
     }
-    function subtractAllocation( address _walletAddress, uint32 _percentCount ) public override(IDirectDonation, PercentRecord) onlyOwner{
-        PercentRecord.subtractAllocation( _walletAddress, _percentCount);
+    function subtractAllocation( address _walletAddress, uint32 _percentCount ) external onlyOwner {
+        require(_percentCount > PercentAllocationMap[_walletAddress], "cannot subtract more than already allocated.");
+        PercentAllocationMap[_walletAddress] -= _percentCount;
+        PercentAllocationSum -= _percentCount;
+        emit LogSubtractAllocation( _walletAddress, _percentCount);
     }
-    function getAllocationValue( address _walletAddress ) public view override(IDirectDonation, PercentRecord) returns(uint32) {
-        return PercentRecord.getAllocationValue( _walletAddress);
+    function getAllocationValue( address _walletAddress ) external view onlyOwner returns(uint32) {
+        return PercentAllocationMap[_walletAddress];
     }
-    function removeAllocation( address _walletAddress ) public override(IDirectDonation, PercentRecord) onlyOwner {
-        PercentRecord.removeAllocation( _walletAddress);
+
+    function getAllocationSum() external view onlyOwner returns(uint32) {
+        return PercentAllocationSum;
+    }
+
+    function removeAllocation( address _walletAddress ) external onlyOwner {
+        PercentAllocationSum -= PercentAllocationMap[_walletAddress];
+        PercentAllocationMap[_walletAddress] = 0;
+        PercentSet.remove(_walletAddress);
+        emit LogRemoveAllocation( _walletAddress);
+    }
+
+    function getAllocationList() external view onlyOwner returns( SimplifiedFormat[] memory ) {
+        SimplifiedFormat[] memory  ret; 
+        address currAddr;
+        for (uint32 i = 0; i< PercentSet.count(); i++){
+            currAddr = PercentSet.keyAtIndex(i);
+            ret[i] = SimplifiedFormat ({
+                walletAddress: currAddr,
+                percentAllocation: PercentAllocationMap[currAddr]
+            });
+        }
+        return ret;
     }
 
     function donate() external payable {
         // send ethers to percentage address    
         //remaining unallocated Percent will be given to Owner
-        uint32 iter = 0;
         uint256 transferable = 0;
-        Allocation memory iterAllocation = Allocation(address(0),0);
-        while (iter < PercentRecord.count){
-            iterAllocation = PercentRecord.allocationMap[iter];
-            if (iterAllocation.objAddress != address(0)){
-                transferable = (msg.value*iterAllocation.percentCount) / 1000000000; 
-                (bool sent, bytes memory data) = iterAllocation.objAddress.call{value: transferable}("");
-                require(sent,"Failed to send Ether");
-            }
-            iter += 1; 
+        address currAddr;
+        for (uint32 i = 0; i< PercentSet.count(); i++){
+            currAddr = PercentSet.keyAtIndex(i);
+            transferable = (msg.value*PercentAllocationMap[currAddr]) / 1000000000;
+            (bool sent, ) = currAddr.call{value: transferable}("");
+            require(sent,"Failed to send Ether");
         }
+        emit LogDonation(msg.value);
     }
 
     function withdrawContractBalance() external onlyOwner{
         uint256 contractBalance = address(this).balance; 
-        (bool sent, bytes memory data) = Ownable.owner().call{value: contractBalance}("");
+        (bool sent, ) = Ownable.owner().call{value: contractBalance}("");
         require(sent,"Failed to send Ether");
+        emit LogWithdrawal(contractBalance);
+    }
+
+    function destory() external onlyOwner{
+        emit LogWithdrawal(address(this).balance);
+        selfdestruct(payable(Ownable.owner()));
     }
 
 }
