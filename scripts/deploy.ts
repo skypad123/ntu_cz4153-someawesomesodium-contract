cf1063f83b65c1fc9896a1b82cece9ce611c7744
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-09-21 15:08:29 +0800

    added script to deploy DirectDonation and Test ERC20

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,66 @@
+
+
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+
+import { expect } from "chai";
 import { ethers } from "hardhat";
 
+import { abi as directDonationABI } from "../artifacts/contracts/DirectDonation.sol/DirectDonation.json";
+
+import { DirectDonation, TestERC20 } from "../typechain-types";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
+import { DirectDonation__factory } from "../typechain-types/factories/contracts/unimplemented_contracts/_DirectDonation.sol";
+import { BigNumber } from "ethers";
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await ethers.getContractFactory("Lock");
 
-  await lock.deployed();
+  var MainDirectDonation: DirectDonation;
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  var MainERC20: TestERC20;
+  var SecondaryERC20: TestERC20;
+
+  var MainOwner: SignerWithAddress;
+  var OtherOwners: SignerWithAddress[];
+
+
+
+  const accounts = await ethers.getSigners();
+  MainOwner = accounts[0];
+  OtherOwners = accounts.slice(1);
+
+  //set DirectDonation Contract Instance
+  const DDcontractCF = await ethers.getContractFactory("DirectDonation");
+  const DDcontract = await DDcontractCF.connect(MainOwner).deploy(await MainOwner.getAddress());
+  MainDirectDonation = DDcontract;
+  console.log("DirectDonationContract");
+  console.log(await MainDirectDonation.address);
+  //set MainERC20 Contract Instance
+  const ERC20contractCF1 = await ethers.getContractFactory("TestERC20");
+  const ERC20contract1 = await ERC20contractCF1.connect(MainOwner).deploy("Token1", "TK1", 1000000000 );
+  MainERC20 = ERC20contract1;
+  console.log("Erc20 main");
+  console.log(await MainERC20.address);
+  //set MainERC20 Contract Instance
+  const ERC20contractCF2 = await ethers.getContractFactory("TestERC20");
+  const ERC20contract2 = await ERC20contractCF2.connect(MainOwner).deploy("Token2", "TK2", 1000000000 );
+  SecondaryERC20 = ERC20contract2;
+  console.log("Erc20 secondary");
+  console.log(await SecondaryERC20.address);
+
+
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  // await lock.deployed();
+
+  // console.log("Lock with 1 ETH deployed to:", lock.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
