406aa7fa3ddb199e4cb46ed716589aaae67eb403
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-10-07 17:23:09 +0800

    reconfigure hardhat config to support dotenv

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     goerli_testnet: {
-        url: `https://goerli.infura.io/v3/<your infura node endpoint>`,
-        accounts: [`0x` + "<your private key>"],
-        gasPrice: 1000
+        url: process.env.INFURA_URL_PROVIDER,
+        accounts: [`0x${process.env.GOERLI_PVT_KEY}`],
+        gasPrice: 5000000000000000000000
     },
+    local_testnet: {
+      url: `http://192.168.1.98:7545`,
+      accounts: [`0x` + "e07ebd366f038ec838c253ce1610af2214a037e1671f4c0ed945bf5177c46dce"],
+      gasPrice: 1000
+  },
   }
 };
 
