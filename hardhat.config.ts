50ec3865f280a9fee8cfe27b3ed7f76a2ae2ada1
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-10-15 21:14:22 +0800

    add erc20 to deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,15 +6,20 @@ dotenv.config()
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    goerli_testnet: {
-        url: process.env.INFURA_URL_PROVIDER,
-        accounts: [`0x${process.env.GOERLI_PVT_KEY}`],
-        gasPrice: 5000000000000000000000
-    },
+    // goerli_testnet: {
+    //     url: process.env.INFURA_URL_PROVIDER,
+    //     accounts: [`0x${process.env.GOERLI_PVT_KEY}`],
+    //     gasPrice: 500000000
+    // },
     local_testnet: {
-      url: `http://192.168.1.98:7545`,
-      accounts: [`0x` + "e07ebd366f038ec838c253ce1610af2214a037e1671f4c0ed945bf5177c46dce"],
-      gasPrice: 1000
+        url: `http://127.0.0.1:8545/`,
+        accounts: ["0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"],
+        gasPrice: 900000000
+    },
+    local_ganache_testnet: {
+      url: `http://127.0.0.1:7545/`,
+      accounts: ["0x3b2f7331c84181e14ad4dca42166dbfa01949e4f44a0cd4a5d4704101b91dba1"],
+      gasPrice: 900000000
   },
   }
 };
--- scripts/deploy_prod_DDManagerFactory.ts
+++ scripts/deploy_prod_DDManagerFactory.ts
@@ -1,12 +1,16 @@
 import { ethers  } from "hardhat";
 
-import { DDManagerFactory} from "../typechain-types";
+import { DDManagerFactory, TestERC20} from "../typechain-types";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 
 
 async function main() {
 
   var MainDDManagerFactory: DDManagerFactory;
+
+
+  var MainERC20: TestERC20;
+  var SecondaryERC20: TestERC20;
 
   const accounts = await ethers.getSigners();
   const MainOwner = accounts[0];
@@ -15,10 +19,21 @@ async function main() {
   //set DDManager Contract Instance
   const DDManagerFactoryContractCF = await ethers.getContractFactory("DDManagerFactory");
   const DDManagerFactoryContract = await DDManagerFactoryContractCF.connect(MainOwner).deploy();
-  MainDDManagerFactory = DDManagerFactoryContract;
-  console.log("DirectDonationManagerFactoryContract");
-  console.log(await MainDDManagerFactory.address);
 
+  console.log("DirectDonationManagerFactoryContract");
+  console.log(await DDManagerFactoryContract.address);
+  //set MainERC20 Contract Instance
+  const ERC20contractCF1 = await ethers.getContractFactory("TestERC20");
+  const ERC20contract1 = await ERC20contractCF1.connect(MainOwner).deploy("Token1", "TK1", 1000000000 );
+  MainERC20 = ERC20contract1;
+  console.log("Erc20 main");
+  console.log(await MainERC20.address);
+  //set MainERC20 Contract Instance
+  const ERC20contractCF2 = await ethers.getContractFactory("TestERC20");
+  const ERC20contract2 = await ERC20contractCF2.connect(MainOwner).deploy("Token2", "TK2", 1000000000 );
+  SecondaryERC20 = ERC20contract2;
+  console.log("Erc20 secondary");
+  console.log(await SecondaryERC20.address);
 
 }
 
