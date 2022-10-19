bda1398cf49b19c240dd1c899aeda5318aa0a51a
Sky Livingston <27288135+skypad123@users.noreply.github.com>
2022-10-19 15:34:55 +0800

    add readme and modified config files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,25 +3,40 @@ import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
+
+function configFile(){
+  let network: any = {};
+
+  if (process.env.INFURA_URL_PROVIDER != undefined && process.env.GOERLI_PVT_KEY != undefined){
+    network["goerli_testnet"] ={
+      url: process.env.INFURA_URL_PROVIDER,
+      accounts: [`${process.env.GOERLI_PVT_KEY}`],
+      gasPrice: 500000000
+    }
+  }
+
+  if(process.env.HARDHAT_PVT_KEY != undefined){
+    network["local_hardhat_testnet"]={
+      url: `http://127.0.0.1:8545/`,
+      accounts: [`${process.env.HARDHAT_PVT_KEY}`],
+      gasPrice: 900000000
+    }
+  }
+
+  if(process.env.GANACHE_PVT_KEY != undefined){
+    network["local_ganache_testnet"]={
+      url: `http://127.0.0.1:7545/`,
+      accounts: [`${process.env.GANACHE_PVT_KEY}`],
+      gasPrice: 900000000
+    }
+  }
+  return network;
+} 
+
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
-  networks: {
-    // goerli_testnet: {
-    //     url: process.env.INFURA_URL_PROVIDER,
-    //     accounts: [`0x${process.env.GOERLI_PVT_KEY}`],
-    //     gasPrice: 500000000
-    // },
-    local_testnet: {
-        url: `http://127.0.0.1:8545/`,
-        accounts: ["0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e"],
-        gasPrice: 900000000
-    },
-    local_ganache_testnet: {
-      url: `http://127.0.0.1:7545/`,
-      accounts: ["0x3b2f7331c84181e14ad4dca42166dbfa01949e4f44a0cd4a5d4704101b91dba1"],
-      gasPrice: 900000000
-  },
-  }
+  networks: configFile()
 };
 
 export default config;
